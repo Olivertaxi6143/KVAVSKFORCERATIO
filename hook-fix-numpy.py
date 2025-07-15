0d4fe9c709e1cb9bd1abf52a472bc2cb9be62eea
Oliver <oliver@gmail.com>
2025-07-15 15:13:24 +0100

    feat: rama ml con solo módulo src/ml actualizado

--- hook-fix-numpy.py
+++
@@ -1,7 +0,0 @@
-import os
-import numpy as _np
-# Filtrar cualquier ruta que acabe en …/numpy/numpy
-_np.__path__ = [
-    p for p in _np.__path__
-    if os.path.basename(os.path.dirname(p)) != 'numpy'
-] 
